f540081f18eaa51ae0e5f5339caa161bb17755a2
Jermaine_Buendia <buendiajermaine@gmail.com>
2026-05-06 23:19:38 +0800

    Add files via upload

---
+++ buyer/config/paymongo.php
@@ -0,0 +1,7 @@
+<?php
+// Paymongo Configuration for Test Mode
+define('PAYMONGO_SECRET_KEY', 'sk_test_nYrxdAdgUq1AA1k2cCzvE84g'); 
+define('PAYMONGO_PUBLIC_KEY', 'pk_test_v7Lz8ZAtfJY7h4rRgaT9rUzv');
+define('PAYMONGO_API_URL', 'https://api.paymongo.com/v1');
+
+?>
